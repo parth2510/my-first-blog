ea46a2de28e87fcf6b8fc3292961bfc72b345a75
parth <parthguptapg7326@gmail.com>
2021-02-07 15:55:37 +0530

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post_detail/<int:pk>/', views.post_detail,name= 'post_detail'),
 ] # see explanation
